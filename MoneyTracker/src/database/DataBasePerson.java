1862e1f794a3c8a05f110aa02d3303acb55ec1fe
chair <chairi.afaf@gmail.com>
2024-12-03 12:45:20 +0100

    DataBasePerson.

--- MoneyTracker/src/database/DataBasePerson.java
+++ MoneyTracker/src/database/DataBasePerson.java
@@ -1,6 +1,22 @@
 package database;
 
-public class DataBasePerson {
+import person.Person;
 
+import java.util.HashSet;
+
+public class DataBasePerson {
+ // heeft set van personen en hoe hasht (hoe zie ik de verscchillende personen door bankrekening)
+    private static HashSet<Person> PersonSet; //singleton pattern
+    private DataBasePerson(){
+        PersonSet = new HashSet<>();
+    }
+
+    public static synchronized HashSet<Person> getInstance(){
+        if (PersonSet == null){
+            PersonSet = new HashSet<>();
+        }
+
+        return PersonSet ;
+    }
 
 }
